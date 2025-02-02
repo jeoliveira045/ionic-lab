7a5fbd65293930cc0c17901b9222fcdb886fac0a
jeoliveira045 <jean.mateus.1997@gmail.com>
2025-02-02 00:29:51 -0300

    Adição do agrupamento do numero de mangas por linha

--- src/app/sidebar/sidebar.page.ts
+++ src/app/sidebar/sidebar.page.ts
@@ -3,13 +3,23 @@ import {
   IonButton,
   IonButtons,
   IonContent,
-  IonHeader, IonItem, IonList,
+  IonHeader,
+  IonItem,
+  IonList,
   IonMenu,
   IonMenuButton,
   IonTitle,
   IonToolbar,
   IonInfiniteScroll,
-  IonInfiniteScrollContent
+  IonInfiniteScrollContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent
 } from "@ionic/angular/standalone";
 import { StatusBar } from "@capacitor/status-bar";
 import {MangaService} from "../services/manga.service";
@@ -29,33 +39,66 @@ import {InfiniteScrollCustomEvent} from "@ionic/angular";
     IonButtons,
     IonMenuButton,
     NgFor,
-    IonList,
-    IonItem,
     IonInfiniteScroll,
-    IonInfiniteScrollContent
+    IonInfiniteScrollContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent
   ],
   standalone: true
 })
 export class SidebarPage{
 
-  mangalist: Array<any> = new Array<any>()
+  mangalist: Array<Array<any>> = new Array<Array<any>>()
 
   page: number = 1
 
+  groupingBy:number = 2
+
   constructor(protected mangaService: MangaService) {
     mangaService.getManga(this.page).subscribe((res: any) => {
-      this.mangalist.push(...res.data)
+      this.mangalist = this.groupMangas(res.data)
     })
+    StatusBar.setOverlaysWebView({overlay: false})
+    console.log(this.mangalist)
   }
 
-  getMoreMangas(){
-    this.page++
+  getMoreMangas() {
+    this.page++;
+
     this.mangaService.getManga(this.page).subscribe((res: any) => {
-      for(let i = 0; i < res.data.length; i++)[
-        this.mangalist.push(res.data[i])
-      ]
-    })
+      const lastGroup = this.mangalist[this.mangalist.length - 1];
+      const remainingData = [...res.data];
+
+      if (lastGroup.length < this.groupingBy) {
+        const elementsToAdd = remainingData.splice(0, this.groupingBy - lastGroup.length);
+        lastGroup.push(...elementsToAdd);
+      }
+
+      if (remainingData.length) {
+        this.mangalist.push(...this.groupMangas(remainingData));
+      }
+
+      console.log(this.mangalist)
+    });
   }
+
+  private groupMangas(data: any[]): any[][] {
+    return data.reduce((groups: any[][], item: any, index: number) => {
+      item.genres = item.genres.join(", ")
+      if (index % this.groupingBy === 0) {
+        groups.push([]);
+      }
+      groups[groups.length - 1].push(item);
+      return groups;
+    }, []);
+  }
+
 
   infiniteScroll(event: InfiniteScrollCustomEvent){
     this.getMoreMangas()
